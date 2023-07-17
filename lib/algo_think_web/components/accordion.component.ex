d54b61a7e043c25cdf41c1256745fd69613265da
Jonas Fischer <fischer-jon@t-online.de>
2023-07-17 16:40:13 +0200

    fixed accordion initially closed

--- lib/algo_think_web/components/accordion.component.ex
+++ lib/algo_think_web/components/accordion.component.ex
@@ -2,18 +2,19 @@ defmodule AlgoThinkWeb.Accordion do
   use Phoenix.Component
 
   attr :header, :string, required: true
+  attr :open, :boolean, default: false
 
   slot :inner_block, required: false
   def accordion(assigns) do
     ~H"""
-    <details class="w-full bg-gray-100 rounded-md p-3 group" open>
-      <summary class="list-none cursor-pointer mb-3">
+    <details class="w-full bg-gray-100 rounded-md p-3 group" open={@open}>
+      <summary class="list-none cursor-pointer">
         <div class="flex justify-between items-center">
           <span class="font-medium text-xl"><%= @header %></span>
           <MaterialIcons.chevron_right class="fill-gray-400 group-open:rotate-90 transition-transform" size={32} />
         </div>
       </summary>
-      <div class="w-full">
+      <div class="mt-3 w-full">
         <%= render_slot(@inner_block) %>
       </div>
     </details>
