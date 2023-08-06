fd97270b2bf568a5b23302b303b639069ae13f2a
Jonas Fischer <fischer-jon@t-online.de>
2023-08-06 11:44:21 +0200

    fixed auto scroll bug

--- assets/js/chatScroll.js
+++ assets/js/chatScroll.js
@@ -3,15 +3,20 @@ const anchor = document.getElementById("chat-anchor")
 
 let scrollAtBottom = true
 
+console.log(chat);
 
 if (chat) {
   // check if scroll is already on the bottom
   chat.addEventListener("scroll", event => {
+    console.log(chat.scrollTop + chat.clientHeight + 100 >= chat.scrollHeight);
     scrollAtBottom = chat.scrollTop + chat.clientHeight + 100 >= chat.scrollHeight
   })
   
   // scroll to bottom if new message available
   window.addEventListener("algothink:newMessage", (event) => {
-    if (scrollAtBottom) anchor.scrollIntoView();
+    // delay so that the element can be added to the dom
+    setTimeout(() => {
+      if (scrollAtBottom) anchor.scrollIntoView()
+    }, 1);
   })
 }
