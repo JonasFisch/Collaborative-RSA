f011d664a71ba5a39d437ad68b9338dbf4b69005
Jonas Fischer <fischer-jon@t-online.de>
2023-08-11 17:41:06 +0200

    added empty state for classroom list

--- lib/algo_think_web/components/core_components.ex
+++ lib/algo_think_web/components/core_components.ex
@@ -435,6 +435,8 @@ defmodule AlgoThinkWeb.CoreComponents do
     attr :label, :string
   end
 
+  slot :alt, required: true
+
   slot :action, doc: "the slot for showing user actions in the last table column"
 
   def table(assigns) do
@@ -454,34 +456,41 @@ defmodule AlgoThinkWeb.CoreComponents do
         </thead>
         <tbody
           id={@id}
-          phx-update={match?(%Phoenix.LiveView.LiveStream{}, @rows) && "stream"}
           class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-zinc-700"
         >
-          <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="group hover:bg-zinc-50">
-            <td
-              :for={{col, i} <- Enum.with_index(@col)}
-              phx-click={@row_click && @row_click.(row)}
-              class={["relative p-0", @row_click && "hover:cursor-pointer"]}
-            >
-              <div class="block py-4 pr-6">
-                <span class="absolute -inset-y-px right-0 -left-4 group-hover:bg-zinc-50 sm:rounded-l-xl" />
-                <span class={["relative", i == 0 && "font-semibold text-zinc-900"]}>
-                  <%= render_slot(col, @row_item.(row)) %>
-                </span>
-              </div>
-            </td>
-            <td :if={@action != []} class="relative w-14 p-0">
-              <div class="relative whitespace-nowrap py-4 text-right text-sm font-medium">
-                <span class="absolute -inset-y-px -right-4 left-0 group-hover:bg-zinc-50 sm:rounded-r-xl" />
-                <span
-                  :for={action <- @action}
-                  class="relative ml-4 font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
-                >
-                  <%= render_slot(action, @row_item.(row)) %>
-                </span>
-              </div>
-            </td>
-          </tr>
+          <%= if length(Enum.to_list(@rows)) > 0 do %>
+            <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="group hover:bg-zinc-50">
+              <td
+                :for={{col, i} <- Enum.with_index(@col)}
+                phx-click={@row_click && @row_click.(row)}
+                class={["relative p-0", @row_click && "hover:cursor-pointer"]}
+              >
+                <div class="block py-4 pr-6">
+                  <span class="absolute -inset-y-px right-0 -left-4 group-hover:bg-zinc-50 sm:rounded-l-xl" />
+                  <span class={["relative", i == 0 && "font-semibold text-zinc-900"]}>
+                    <%= render_slot(col, @row_item.(row)) %>
+                  </span>
+                </div>
+              </td>
+              <td :if={@action != []} class="relative w-14 p-0">
+                <div class="relative whitespace-nowrap py-4 text-right text-sm font-medium">
+                  <span class="absolute -inset-y-px -right-4 left-0 group-hover:bg-zinc-50 sm:rounded-r-xl" />
+                  <span
+                    :for={action <- @action}
+                    class="relative ml-4 font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
+                  >
+                    <%= render_slot(action, @row_item.(row)) %>
+                  </span>
+                </div>
+              </td>
+            </tr>
+          <% else %>
+            <tr>
+              <td colspan="2">
+                <%= render_slot(@alt) %>
+              </td>
+            </tr>
+          <% end %>
         </tbody>
       </table>
     </div>
